57288f5f3b097bf8438910e329b8fe745c59ad36
Warakorn Jetlohasiri <wadh1n1@hotmail.com>
2021-04-01 04:27:25 +0700

    Recovering the api

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,281 @@
-const functions = require("firebase-functions");
+/* eslint-disable promise/always-return */
+// List of dependencies used in the code
+const functions = require('firebase-functions');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const bodyParser = require("body-parser");
+
+// The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
-const app = express();
 
-var serviceAccount = require("./permission.json");
+// Module handling on csv file and file system
+const csv = require('csv-parser');
+const fs = require('fs');
+
+// Module working on file uploads
+const formidable = require("formidable");
+
+var hbs = require('handlebars');
+var engines = require('consolidate');
+
+const path = require('path');
+
+// Initialize firebase application
+var serviceAccount = require('./service_account.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://sadao-f4a1e.firebaseio.com/"
+    databaseURL: "https://sadao-f4a1e.firebaseio.com"
+});
+const app = express();
+const csrfMiddleware = csrf({cookie: true});
+
+app.engine('hbs', engines.handlebars);
+app.set('views', path.join(__dirname, "views/"));
+app.set('view engine', 'hbs');
+
+
+app.use(cors({ origin: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(csrfMiddleware);
+let token;
+
+app.all("*", (req, res, next) => {
+    token = req.csrfToken();
+    res.cookie("XSRF-TOKEN", token);
+    res.locals._csrf = token;
+    next();
 });
 
-const firestoreDB = admin.firestore();
+const db = admin.database();
 
-app.use(cors({
-        origin: true
-}));
+const SAMPLES = "samples";
+const raw_sample_db = admin.firestore();
+const rawSampleDB = raw_sample_db.collection(SAMPLES);
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send("Hello World");
+
+// API handling Authentication , Login, Logout
+app.get('/login', async(req, res) => {
+    // const snapshot = await rawSampleDB.get();
+    // snapshot.forEach((doc) => {
+    //     console.log(doc.id, '=>', doc.data());
+    // });
+    res.render('login', {csrfToken: token});
+});
+
+app.get('/signup', async(req, res) => {
+    res.render('registration');
+});
+
+app.post("/sessionLogin", (req, res) => {
+    const idToken = req.body.idToken;
+    const csrfToken = req.body.csrfToken;
+    let csrfToken_session = token;
+    console.log("csrfToken: " + csrfToken);
+    console.log("session csrf: " + csrfToken_session);
+
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+    // eslint-disable-next-line promise/catch-or-return
+    admin.auth().createSessionCookie(idToken, {expiresIn}).then(
+        (sessionCookie) => {
+            const options = { maxAge: expiresIn, httpOnly: true};
+            res.cookie("session", sessionCookie, options);
+            res.end(JSON.stringify({status:"success"}));
+        },
+        (error) => {
+            res.status(401).send("UNAUTHORISED REQUEST");
+        }
+    );
+});
+
+// Handle Logout
+app.get('/logout', (req, res) =>  {
+    res.clearCookie("session");
+    res.redirect('/login');
 })
 
+// API handling heatmap page
+app.get('/', async (req, res) => {
+    res.render('heatmap');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // let sessionCookie = req.cookies.session || "";
+    // admin
+    // .auth()
+    // .verifySessionCookie(sessionCookie, true)
+    // .then(() => {
+    //     res.render('heatmap');
+    // })
+    // .catch((error) => {
+    //     res.redirect("/login");
+    // });
+    
+});
+
+// API handling administration on each samples
+app.get('/samples', async (req,res) => {
+    res.render('admin', {csrfToken: token});
+    
+    // let sessionCookie = req.cookies.session || "";
+
+    // admin
+    // .auth()
+    // .verifySessionCookie(sessionCookie, true)
+    // .then(() => {
+    //    res.render('admin', {csrfToken: token});
+    // })
+    // .catch((error) => {
+    //    res.redirect("/login");
+    // });  
+});
+
+
+// Update the sample information corresponding to input attrs
+app.put('/samples/:sampleId', (req, res) => {
+    const sampleId = req.params.sampleId;
+    let sampleRef = db.ref('sampling_data/' + sampleId);
+    sampleRef.update(req.body);
+
+    console.log("sample " + sampleId + " has been edited");
+    res.send(req.body);
+    res.render("admin");
+});
+
+// Insert new sample to the database
+app.post("/samples", (req, res) => {
+    let body = req.body;
+    let sampleId = (body.sampleId).replace(/[&/\\#,+()$~%.'":*?<>{}]/g,'_');
+    console.log("sampleId: "  + sampleId);
+    let sampleRef = db.ref('sampling_data/'+ sampleId);
+    sampleRef.set({
+        "Ca": parseFloat(body.Ca),
+        "EC": parseFloat(body.EC), 
+        // "K": parseFloat(body.K) ,
+        "NO3": parseFloat(body.NO3),
+        "collectedDate": body.collectedDate,
+        "latitude": parseFloat(body.latitude),
+        "location": body.location,
+        "longitude":parseFloat(body.longitude),
+        "pH": parseFloat(body.pH),
+    });
+
+    console.log("Data has been saved");
+
+    console.log(req.body);
+    res.render('admin');
+});
+
+//  Delete the sample
+app.delete('/samples/:sampleId', (req, res) => {
+    const sampleId = req.params.sampleId;
+    let sampleRef = db.ref('sampling_data/' + sampleId);
+    sampleRef.remove();
+    res.render("admin");
+});
+
+// API for handling raw samples (has not been measured yet)
+app.get('/samples/raw', (req, res) => {
+    res.render('sampleRaw',  {csrfToken: token});
+});
+
+// Insert new sample to the database
+app.post("/samples/raw", (req, res) => {
+    let body = req.body;
+    let sampleId = (body.sampleId).replace(/[&/\\#,+()$~%.'":*?<>{}]/g,'_');
+    let sampleRef = db.ref('sampling_data/'+ sampleId);
+    sampleRef.set({
+        "Ca": parseFloat(body.Ca),
+        "EC": parseFloat(body.EC), 
+        "K": parseFloat(body.K) ,
+        "NO3": parseFloat(body.NO3),
+        "collectedDate": body.collectedDate,
+        "latitude": parseFloat(body.latitude),
+        "location": body.field,
+        "longitude":parseFloat(body.longitude),
+        "pH": parseFloat(body.pH),
+    });
+
+    console.log("Data has been saved");
+
+    console.log(req.body);
+    res.render('sampleRaw');
+});
+
+//  Delete the sample
+app.delete('/samples/raw/:sampleId', (req, res) => {
+    const sampleId = req.params.sampleId;
+    let sampleRef = rawSampleDB.where("sampleName","==", sampleId);
+    // eslint-disable-next-line promise/catch-or-return
+    sampleRef.get().then(function(querySnapshot){
+        querySnapshot.forEach((doc) => {
+            doc.ref.delete();
+        });
+    });
+    res.render("sampleRaw");
+});
+
+
+
+// API for handling uploading file data to database
+app.get('/heatmap/upload', (req, res) => {
+    res.render('heatmapFile', {csrfToken: token});
+});
+
+app.post('/heatmap/upload', (req, res) => {
+    res.render('heatmapFile');
+});
+
+// API for handling parameters
+app.get('/settings', (req, res) => {
+    res.render('settings', {csrfToken: token});
+    
+    // let sessionCookie = req.cookies.session || "";
+
+    // admin.auth()
+    // .verifySessionCookie(sessionCookie, true)
+    // .then(() => {
+    //     res.render('settings', {csrfToken: token});
+    // })
+    // .catch((error) => {
+    //    res.redirect("/login");
+    // });
+});
+
+app.put('/settings/:parameterName', (req,res) => {
+    const paramName = req.params.parameterName;
+    let paramRef = db.ref('parameters/' + paramName);
+    paramRef.update({
+        min: req.body.min,
+        max: req.body.max
+    });
+    res.send(req.body);
+    res.render("settings");
+});
+
+// Get Min Max from specified parameter
+app.get('/parameters/:parameterName', (req,res) =>{
+    const paramName = req.params.parameterName;
+    let paramRef = db.ref('parameters/' + paramName);
+    res.send(paramRef.once('value'));
+});
+
+// API endpoint for retrieving zone in Sadao to be rendered on heatmap
+exports.app = functions.https.onRequest(app);
+
+exports.zoningMap = functions.https.onRequest((request, response) => {
+    var zoning_coordinates = [];
+    response.set('Access-Control-Allow-Origin', '*');
+    fs.createReadStream('./sadao_zone.csv').pipe(csv())
+    .on('data', (data) => 
+    zoning_coordinates.push(data))
+    .on('end', () => {response.send(zoning_coordinates);});
+
+});
+
+
+
+
+
